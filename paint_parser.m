72ed8bee534344f7fe78933ce073eafee44872e9
ANDRES ULLOA (RIT Student) <adu8917@mail.rit.edu>
2016-05-06 14:55:05 -0400

    andded find_blocks and frames output

--- paint_parser.m
+++ paint_parser.m
@@ -1,16 +1,24 @@
 % painting_parser
-close all
-tic
-%^ total 17 minutes
-ref_image = imresize(im2double(imread('2.png')),.6);
+% close all
+% tic
+% %^ total 17 minutes
+% ref_image = imresize(im2double(imread('2.png')),.6);
+% 
+% v      = VideoReader('output1.avi');
+% video  = read(v,[1 inf]);
+% tic
+% % runs in ten minutes
+% frames = frames_containing_image(ref_image,video,4);
+% toc
 
-v = VideoReader('output1.avi');
-video = read(v,[1 inf]);
-tic
-% runs in ten minutes
-frames = frames_containing_image(ref_image,video,4);
-toc
-for i = 230:239
-    figure;
-    imshow(video(:,:,:,frames(i)))
-end
+
+
+save('frames_output.txt','frames','-ascii')
+
+[blocks_start,blocks_end] = find_blocks(frames);
+
+
+% for i = 230:239
+%     figure;
+%     imshow(video(:,:,:,frames(i)))
+% end
