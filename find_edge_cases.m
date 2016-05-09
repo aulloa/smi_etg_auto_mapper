c2b2503f23b6a50122b31b238a03927330e67c76
ANDRES ULLOA (RIT Student) <adu8917@mail.rit.edu>
2016-05-09 13:38:51 -0400

    edge cases

---
+++ find_edge_cases.m
@@ -0,0 +1,5 @@
+for i  = 1:10
+    edge_case_index = 
+    if blocks_start(i)+i
+    edge_cases(i) = frames(blocks_start(i)+i)
+end
--- paint_parser.m
+++ paint_parser.m
@@ -16,7 +16,12 @@
 save('frames_output.txt','frames','-ascii')
 
 [blocks_start,blocks_end] = find_blocks(frames);
-
+for i = 1:length(blocks_start)
+    frame_to_show = Blocks_start(i)
+    imshow(video(frame_to_show))
+    include_ind = menu('Is painting in frame?','Yes','No')
+end
+    
 
 % for i = 230:239
 %     figure;
