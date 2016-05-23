d2c9432577c251ae8ded4f50b6b99c22f7bf9b28
aulloa <adu8917@rit.edu>
2016-05-23 18:29:39 -0400

    integrated find_edge_cases into paint_parser

--- paint_parser.m
+++ paint_parser.m
@@ -15,15 +15,14 @@ frames = frames_containing_image(ref_image,video,4);
 save('frames_output.txt','frames','-ascii')
 
 [blocks_start,blocks_end] = find_blocks(frames);
+last_frame   = frames(end)
+num_edge_cases = 5
+edge_cases = find_edge_cases(num_edge_cases,last_frame,blocks_start,blocks_end)
 
-for i = 1:length(blocks_start)
+
+for i = 1:length(edge_cases)
     frame_to_show = edge_cases(i)
-    imshow(video(frame_to_show))
+    imshow(video(:,:,:,frame_to_show))
     include_ind = menu('Is painting in frame?','Yes','No')
 end
-    
-
-% for i = 230:239
-%     figure;
-%     imshow(video(:,:,:,frames(i)))
-% end
+close all
